bf1efb213bfab02ca189266c1fc25a0592238096
davidrfreeman <davidfreeman@holos.solutions>
2017-05-13 15:49:14 -0600

    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # js30
+
+### Repo to test and learn/re-learn JS without using any frameworks or libraries
+
+### I will use the JavaScript 30 idea as a guide for projects and also other ideas I may get along the way
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   let btn = document.querySelector("#btn");
   btn.addEventListener("click", function() {
-    document.body.style.backgroundColor = "blue";
-  })
+    
+  });
 });
