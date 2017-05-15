486bdf0819021bd64e44e7e0c2bd0e6ef1aa16f9
davidrfreeman <davidfreeman@holos.solutions>
2017-05-14 22:19:25 -0600

    began basic quote page

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+  let quoteArr = [
+    {
+      quote: "Hello World",
+      credit: "God"
+    },
+    {
+      quote: "The back of yo head is rediculous!",
+      credit: "Wisest man to walk the earth"
+    }
+  ];
+
+
+  let randoNumber = () => {
+    return Math.round(Math.random());
+  };
+
   let btn = document.querySelector("#btn");
+
   btn.addEventListener("click", function() {
-    
+    let storedNumber = randoNumber();
+    return document.getElementById('display').innerHTML = quoteArr[storedNumber].quote,
+      document.getElementById('quote').innerHTML = quoteArr[storedNumber].credit,
+      console.log(storedNumber);
   });
 });
